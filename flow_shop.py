5eadc7fbc430ea7103774451ac4fae7d5ed539e3
Konzum59 <jakub.mamut@gmail.com>
2026-04-07 17:00:09 +0200

    feat: small improvements

--- flow_shop.py
+++ flow_shop.py
@@ -2,25 +2,30 @@ from RandomNumberGenerator import RandomNumberGenerator
 import numpy as np
 import math
 
-def count_time(processing_times_array):
-    for row in range(number_of_items):
-        for col in range(number_of_machines):
-            if row==0 and col==0:
-                continue
-            left = processing_times_array[row-1][col] if row else 0
-            top = processing_times_array[row][col-1] if col else 0
+def count_time(array, permutation):
+    processing_times_array=array[permutation]
+    for row in range(1, number_of_items):
+        processing_times_array[row][0]+=processing_times_array[row-1][0]
+    for col in range(1, number_of_machines):
+            processing_times_array[0][col]+=processing_times_array[0][col-1]
+    for row in range(1 , number_of_items):
+        for col in range(1 ,number_of_machines):
+
+            left = processing_times_array[row-1][col]
+            top = processing_times_array[row][col-1]
             processing_times_array[row][col] += max(top,left)
     print(processing_times_array)
     Cmax=processing_times_array.max()
-    return Cmax
+    return Cmax ,permutation
 
 if __name__ == "__main__":
     rnd = RandomNumberGenerator(831764)
 
-    number_of_items=7
-    number_of_machines=5
+    number_of_items=4
+    number_of_machines=3
     items = np.array([[rnd.nextInt(1, 35) for i in range(number_of_machines)] for j in range(number_of_items)])
-    best= math.inf
-    best = min( best , count_time(np.copy(items)))
+    new_order = [2, 0, 1, 3]
+    best = math.inf
+    best = min( best , count_time(items, new_order)[0])
     print(items)
     print(best)
