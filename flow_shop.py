d0904582f3bb903ee5fcf1739dfe5b85da6d88e2
Konzum59 <jakub.mamut@gmail.com>
2026-04-07 17:06:03 +0200

    feat: small improvements

--- flow_shop.py
+++ flow_shop.py
@@ -16,8 +16,7 @@ def count_time(array, permutation):
             processing_times_array[row][col] += max(top,left)
     print(processing_times_array)
     Cmax=processing_times_array.max()
-    return Cmax ,permutation
-
+    return Cmax
 if __name__ == "__main__":
     rnd = RandomNumberGenerator(831764)
 
@@ -25,7 +24,7 @@ if __name__ == "__main__":
     number_of_machines=3
     items = np.array([[rnd.nextInt(1, 35) for i in range(number_of_machines)] for j in range(number_of_items)])
     new_order = [2, 0, 1, 3]
-    best = math.inf
-    best = min( best , count_time(items, new_order)[0])
+    best = math.inf ,new_order
+    best = [min( best[0] , count_time(items, new_order)), new_order]
     print(items)
-    print(best)
+    print(best[0], best[1])
